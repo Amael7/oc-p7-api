dc75e281b21e4182cc15729a6b41679e79c7a0cf
M.Stéphane <stephane.montoro@hotmail.com>
2022-07-08 01:02:22 +0200

    Update ClientController.php

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ClientRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,11 +10,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class ClientController extends AbstractController
 {
     #[Route('/api/clients', name: 'clients', methods: ['GET'])]
-    public function index(): JsonResponse
+    public function index(ClientRepository $clientRepository): JsonResponse
     {
+
+        $clientsList = $clientRepository->findAll();
+
         return new JsonResponse([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/ClientController.php',
+            'clients' => $clientsList,
         ]);
     }
 }
