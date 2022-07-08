6ed45ff7bbcf9c339225d6b432e4f44c5a74cb11
M.Stéphane <stephane.montoro@hotmail.com>
2022-07-08 11:54:55 +0200

    ClientController Destroy method

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Client;
 use App\Repository\ClientRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -21,9 +22,18 @@ class ClientController extends AbstractController
     }
 
     #[Route('/api/clients/{id}', name: 'clientShow', methods: ['GET'])]
-    public function Show(Client $client, ClientRepository $clientRepository, SerializerInterface $serializer): JsonResponse
+    public function show(Client $client, SerializerInterface $serializer): JsonResponse
     {
-        $jsonClient = $serializer->serialize($client, 'json');
-        return new JsonResponse($jsonClient, Response::HTTP_OK, ['accept' => 'json'], true);  # Response 200 if OK and 404
+        $jsonClient = $serializer->serialize($client, 'json', ['groups' => 'getClients']);
+        return new JsonResponse($jsonClient, Response::HTTP_OK, ['accept' => 'json'], true);  # Response 200 if OK and 404 if not found
+    }
+
+    #[Route('/api/clients/{id}', name: 'clientDestroy', methods: ['DELETE'])]
+    public function destroy(Client $client, EntityManagerInterface $em): JsonResponse 
+    {
+        $em->remove($client);
+        $em->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT); # Response 204 - No content
     }
 }
