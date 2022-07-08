4ff2fe5cebbd56474a0127e74cbdc279f80d2e0c
M.Stéphane <stephane.montoro@hotmail.com>
2022-07-08 15:32:15 +0200

    add show method customerController

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Customer;
 use App\Repository\CustomerRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,4 +19,11 @@ class CustomerController extends AbstractController
             $jsoncustomersList = $serializer->serialize($customersList, 'json', ['groups' => ['getCustomerDetails', 'getClientsFromCustomer', 'getClientDetails']]);
             return new JsonResponse($jsoncustomersList, Response::HTTP_OK, [], true);  # Response 200
         }
+
+    #[Route('/api/customers/{id}', name: 'customerShow', methods: ['GET'])]
+        public function show(Customer $customer, SerializerInterface $serializer): JsonResponse
+            {
+                $jsonCustomer = $serializer->serialize($customer, 'json', ['groups' => ['getCustomerDetails', 'getClientsFromCustomer', 'getClientDetails']]);
+                return new JsonResponse($jsonCustomer, Response::HTTP_OK, ['accept' => 'json'], true);  # Response 200 if OK and 404 if not found
+            }
 }
