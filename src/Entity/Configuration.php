3d222d55cfde9ba00f55f6c8ae1610d6d9f00a88
M.Stéphane <stephane.montoro@hotmail.com>
2022-07-14 00:51:50 +0200

    Update Configuration.php

--- src/Entity/Configuration.php
+++ src/Entity/Configuration.php
@@ -6,7 +6,6 @@ use Doctrine\ORM\Mapping as ORM;
 use App\Repository\ConfigurationRepository;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
-
 use JMS\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
